5418683fe8cde30a40fb9a0bc55d8065b17481a9
ahmet.sariyer <ahmet.sariyer@kafein.com.tr>
2018-11-23 11:56:53 +0300

    Menu ve diğer sayfalar düzenlendi. Önyüz geliştirmeleri

--- UskudarDenetim.UI/Controllers/HomeController.cs
+++ UskudarDenetim.UI/Controllers/HomeController.cs
@@ -25,17 +25,14 @@ namespace UskudarDenetim.UI.Controllers
 
         public ActionResult About()
         {
-            //Address tablosunu GetAddresses methodu yardımıyla businesstan talep ettik.Business katmanı repo katmanına istek atarak Tüm adresleri çekti.
-            AddressBusiness business = new AddressBusiness();
-            var model =   business.GetAddresses().FirstOrDefault();
-            return View(model);
+            return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
+
     }
 }
