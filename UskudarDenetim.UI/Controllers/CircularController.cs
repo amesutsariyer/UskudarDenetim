51f4de084306bfebd6f14910c67052756f098d70
ahmet.sariyer <ahmet.sariyer@kafein.com.tr>
2018-12-11 11:31:38 +0300

    Sirküler sayfası eklendi

---
+++ UskudarDenetim.UI/Controllers/CircularController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UskudarDenetim.UI.Controllers
+{
+    public class CircularController : BaseController
+    {
+        // GET: Blog
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
